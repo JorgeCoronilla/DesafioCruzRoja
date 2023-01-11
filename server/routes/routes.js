f4f9c457f8a0b7940879b42efc71e3bb750a6c24
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2023-01-11 16:26:05 +0100

    Estilos ordenados y  welcome y home components separados

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
-<<<<<<< HEAD
 const Logincontroller = require('../controllers/login.controllers')
 
 
 router.post("/recover-pass", Logincontroller.Login.recover)
 router.post("/change-pass", Logincontroller.Login.change_pass)
 router.post("/login", Logincontroller.Login.login)
-=======
 const RegisterController = require('../controllers/register.controllers')
 const jwt = require("jsonwebtoken");
 
