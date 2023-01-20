b308f84dfea862a70755091a03b8c4b6bf8e6959
gmirg <gofthet@gmail.com>
2023-01-20 05:41:00 +0100

    cambios para presentacion en  local

--- server/ddbb/sql/index.js
+++ server/ddbb/sql/index.js
@@ -1,46 +1,27 @@
 //Conexión a MySQL con Sequelize
 const { Sequelize } = require('sequelize');
-const fs = require('fs');
+// const fs = require('fs');
 
 
 
-const serverCa = fs.readFile(`${__dirname}/DigiCertGlobalRootCA.crt.pem`, 'utf8', (err, data) => {
-     if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('ssl cert sent');
-  });
-
-const pool = new Sequelize(
-    'dbtest',
-    process.env.USER,
-    process.env.PASS, {
-    host: process.env.HOST,
-    port: 3306,
-    dialect: 'mysql',
-   dialectOptions: {  ssl: {
-        ca: serverCa
-     }},
-    pool: {
-        max: 100,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-}
-);
-
-module.exports = pool;
-
+// const serverCa = fs.readFile(`${__dirname}/DigiCertGlobalRootCA.crt.pem`, 'utf8', (err, data) => {
+//      if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log('ssl cert sent');
+//   });
 
 // const pool = new Sequelize(
-//     'dbTest',
-//     'root',
-//     'rootroot', {
+//     'dbtest',
+//     process.env.USER,
+//     process.env.PASS, {
 //     host: process.env.HOST,
 //     port: 3306,
 //     dialect: 'mysql',
+//    dialectOptions: {  ssl: {
+//         ca: serverCa
+//      }},
 //     pool: {
 //         max: 100,
 //         min: 0,
@@ -49,4 +30,23 @@ module.exports = pool;
 //     }
 // }
 // );
+
 // module.exports = pool;
+
+
+const pool = new Sequelize(
+    'dbTest',
+    'root',
+    'rootroot', {
+    host: process.env.HOST,
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+        max: 100,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
+);
+module.exports = pool;
